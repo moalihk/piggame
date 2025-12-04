ce65da559f77583f25f6973b299f4263af98a4f2
moalihk <alimohatasimbd@gmail.com>
2025-12-05 00:34:05 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,3 +4,10 @@ from src.game import Game
 class PigDiceShell(cmd.Cmd):
     intro = "Welcome to Pig Dice! Type help or ? to list commands."
     prompt = "(pigdice) "
+
+
+
+
+    def __init__(self):
+        super().__init__()
+        self.game = None
